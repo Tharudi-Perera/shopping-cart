08f0ac8611736cb2d1b750c6e8ade6a07f574d00
Tharudi-Perera <tharudip@auradot.com>
2024-08-08 17:33:24 +0530

    updated

--- backend/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ backend/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -4,6 +4,8 @@ import com.luv2code.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+// Accept calls from web browser scripts for this origin
+
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
